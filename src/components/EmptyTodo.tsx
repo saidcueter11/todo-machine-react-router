0bdbed0d881229252b35447169ae6fab72db6eb7
Said Cueter <rattion@pop-os.localdomain>
2023-01-31 14:37:51 -0500

    Changed Empty page Layout

--- src/components/EmptyTodo.tsx
+++ src/components/EmptyTodo.tsx
@@ -1,3 +1,13 @@
+import { RiTodoFill } from 'react-icons/ri'
+
 export const EmptyTodo = () => {
-  return <p>Aqui no hay nada compadre</p>
+  return (
+    <div className='w-full h-full grid place-content-center mb-24'>
+      <div className='w-full h-full flex flex-col gap-6'>
+        <RiTodoFill size={60} fill='white' className='m-auto' />
+        <p className='text-center font-semibold text-xl text-gray-50'>You do not have Todos pending</p>
+
+      </div>
+    </div>
+  )
 }
--- src/components/TodoSearch.tsx
+++ src/components/TodoSearch.tsx
@@ -1,13 +1,17 @@
+import { useSearchParams } from 'react-router-dom'
 import { ContextType } from '../types'
 
-export const TodoSearch = ({ searchValue, handleNewSearch, loading }:Pick<ContextType, 'searchValue' | 'handleNewSearch' | 'loading'>) => {
+export const TodoSearch = ({ searchValue, handleNewSearch, loading }: Pick<ContextType, 'searchValue' | 'handleNewSearch' | 'loading'>) => {
+  const [search, setSearch] = useSearchParams()
   const handleChange = ({ value }: HTMLInputElement) => {
     handleNewSearch(value)
+    setSearch({ search: value })
+    console.log(search)
   }
 
   return (
     <div className='w-full flex justify-center'>
-      <input onChange={(e) => handleChange(e.target) } type="text" value={searchValue} className="border shadow w-3/4 rounded-md p-1 disabled:animate-pulse" disabled={loading}/>
+      <input onChange={(e) => handleChange(e.target)} type="text" value={searchValue} className="border shadow w-3/4 rounded-md p-1 disabled:animate-pulse" disabled={loading} />
     </div>
   )
 }
